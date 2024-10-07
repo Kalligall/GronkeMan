28a698718399f271d422044cee046177d8a07639
Rickard Edberg <rickard.edberg@gmail.com>
2024-10-07 15:27:06 +0200

    added gitignore and baseline files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //ett konsolspel där du går in i rum som är 10x10 med olika objects och enemies/traps i dessa rum.
+    }
+}
